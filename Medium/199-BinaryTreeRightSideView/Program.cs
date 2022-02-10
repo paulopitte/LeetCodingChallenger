49d7a5c6c29399344326a82194a647e6f77811f8
Paulo Pitte <paulopitte@gmail.com>
2022-02-10 15:55:45 -0300

    cleanUP

--- Medium/199-BinaryTreeRightSideView/Program.cs
+++ Medium/199-BinaryTreeRightSideView/Program.cs
@@ -6,7 +6,29 @@ namespace _199_BinaryTreeRightSideView
 {
 
     // 199. Binary Tree Right Side View
+    /*https://leetcode.com/problems/binary-tree-right-side-view/
+     * Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+    
 
+        Example 1:
+        Input: root = [1,2,3,null,5,null,4]
+        Output: [1,3,4]
+
+
+        Example 2:
+        Input: root = [1,null,3]
+        Output: [1,3]
+        Example 3:
+
+        Input: root = []
+        Output: []
+ 
+
+        Constraints:
+
+        The number of nodes in the tree is in the range [0, 100].
+        -100 <= Node.val <= 100
+     */
     internal class Program
     {
         static void Main(string[] args)
@@ -18,6 +40,7 @@ namespace _199_BinaryTreeRightSideView
             WriteLine("---------------------------------------");
 
 
+            //Example 1:
             TreeNode tree = new(1);
             tree.right = new TreeNode(2)
             {
@@ -30,16 +53,10 @@ namespace _199_BinaryTreeRightSideView
 
             tree.left = new TreeNode(2)
             {
-                right = new TreeNode(5)
-                {
-                    left = new TreeNode(21)
-                    {
-                        left = new TreeNode(75)
-                    }
-                }
+                right = new TreeNode(5)                
             };
 
-
+            // Example 2:
             // tree.left = new();
             // tree.right = new(3);
 
