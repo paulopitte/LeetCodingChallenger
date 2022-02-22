6f2552fc4c8073f404c8f567f818317ab725349e
Paulo Pitte <paulopitte@gmail.com>
2022-02-22 17:27:25 -0300

    01-Find_Missing

---
+++ AmazonTest/01-FindTheMissingNumberInTheArray/Program.cs
@@ -0,0 +1,63 @@
+﻿using static System.Console;
+
+namespace AmazonTest
+{
+    /*
+     
+            Você recebe uma matriz de números positivos de 1to n, de modo que todos os números de 1to nestejam presentes, exceto um número x. 
+            Você tem que encontrar x. A matriz de entrada não está classificada. Olhe para a matriz abaixo e experimente antes de verificar a solução.    
+            
+            Complexidade do tempo de execução: linear) O( n )
+
+            Complexidade da Memória: Constante, O(1)  
+
+            Uma solução ingênua é simplesmente procurar por cada inteiro entre 1e nna matriz de entrada, interrompendo a pesquisa assim que houver um número ausente. Mas nós podemos fazer melhor. Aqui é linear,Sobre)O ( n ), solução que usa a fórmula da soma da série aritmética. Aqui estão as etapas para encontrar o número ausente:
+
+            Encontre a soma sum_of_elementsde todos os números na matriz. Isso exigiria uma varredura linear,Sobre)O ( n ).
+            Em seguida, encontre a soma expected_sumdos primeiros nnúmeros usando a fórmula da soma da série aritmética
+            A diferença entre eles, ou seja expected_sum - sum_of_elements, é o número ausente na matriz.
+    
+     
+     */
+
+
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 3,7,1,2,8,4,5,6 };
+
+            WriteLine("");
+            WriteLine("+++++++++++++++++++++++++++++++++++++++");
+            WriteLine("Find the missing number in the array ");
+
+            WriteLine("---------------------------------------");
+            WriteLine("Result : " + Solution.Find_Missing(arr));
+
+            ReadKey();
+        }
+    }
+
+
+    class Solution
+    {
+
+        public static int Find_Missing(int[] input)
+        {
+            // in input list
+            int sum_of_elements = 0;
+           foreach(int element in input)
+            {
+                sum_of_elements += element;
+            }
+
+            // There is exactly 1 number missing 
+            int n = input.Length + 1;
+            int actual_sum = (n * (n + 1)) / 2;
+            return actual_sum - sum_of_elements;
+        }
+
+    }
+
+
+}
---
+++ AmazonTest/02-DetermineIfTheSumOfTwoIntegersIsEqualToTheGivenValue/Program.cs
@@ -0,0 +1,61 @@
+﻿using static System.Console;
+
+namespace AmazonTest
+{
+    /*
+     
+
+        Dado um array de inteiros e um valor, determine se existem dois inteiros no array cuja soma seja igual ao valor dado.
+        Retorna true se a soma existir e retornar false se não existir. 
+        Considere esta matriz e as somas de destino:
+
+        Complexidade do tempo de execução: linear)O ( n )
+
+        Complexidade da Memória: Linear)O ( n )
+
+        Você pode usar o algoritmo a seguir para encontrar um par que se soma ao destino (digamos val).
+
+        Digitalize todo o array uma vez e armazene os elementos visitados em um conjunto de hash.
+        Durante scan, para cada elemento edo array, verificamos se val - eestá presente no conjunto de hash, ou val - eseja, já foi visitado.
+        Se val - efor encontrado no conjunto de hash, significa que há um par ( e, val - e) no array cuja soma é igual ao dado val.
+        Se esgotamos todos os elementos do array e não encontramos nenhum desses pares, a função retornaráfalse    */
+
+
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 5, 7, 1, 2, 8, 4, 3 };
+
+            WriteLine("");
+            WriteLine("+++++++++++++++++++++++++++++++++++++++");
+            WriteLine("Find the missing number in the array ");
+
+            WriteLine("---------------------------------------");
+            WriteLine("Result : " + Solution.FindSumOfTwo(arr, 9));
+
+            ReadKey();
+        }
+    }
+
+
+    class Solution
+    {
+
+        public static bool FindSumOfTwo(int[] arr, int target)
+        {
+            HashSet<int> found = new();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (found.Contains(target - arr[i]))
+                    return true;
+
+                found.Add(arr[i]);
+            }
+            return false;
+        }
+
+    }
+
+
+}
